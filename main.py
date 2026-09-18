648e1acb7c820fc5f9945892a6d3e187a17edbff
roverram <krishnaramjay2005@gmail.com>
2026-09-18 16:10:57 +0530

    changes from branch2

---
+++ main.py
@@ -0,0 +1,12 @@
+# Inside main.py
+from MiniProject.filters.filters import MinimumEmployeeCompany, MaximumEmployeeCompany, ParticularDomainFind, ParticularIndustryFind
+from MiniProject.jsonread import companies
+print(f"The Companies which is having minimum no of employees are {MinimumEmployeeCompany(companies)}")
+print()
+print(f"The Companies having maximum no of emplopyees are {MaximumEmployeeCompany(companies)}")
+print()
+print(ParticularIndustryFind(companies))
+print()
+print(ParticularDomainFind(companies))
+print("This is the changes from branch1")
+print("This is from branch2")
