da432591b424c4d93de5c5093de49dea4151f80b
roverram <krishnaramjay2005@gmail.com>
2026-09-18 16:28:14 +0530

    changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Inside main.py
 from MiniProject.filters.filters import MinimumEmployeeCompany, MaximumEmployeeCompany, ParticularDomainFind, ParticularIndustryFind
 from MiniProject.jsonread import companies
+print()
 print(f"The Companies which is having minimum no of employees are {MinimumEmployeeCompany(companies)}")
 print()
 print(f"The Companies having maximum no of emplopyees are {MaximumEmployeeCompany(companies)}")
@@ -8,3 +9,4 @@ print()
 print(ParticularIndustryFind(companies))
 print()
 print(ParticularDomainFind(companies))
+print()
